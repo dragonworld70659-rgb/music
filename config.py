766e7e41bc473aee5949a7fd0d3b9a3bd9089310
KittuOP00 <toxic.pap.a00@gmail.com>
2025-11-06 11:12:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 10000))
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
-# Get this value from @purvi_music_bot on Telegram by /id
+# Get this value from @kittuxmusicc_bot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 7403621976))
 
 ## Fill these variables if you're deploying on heroku.
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/shona_bots")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+5hdg36Lda6QzM2M9")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/kittuu_updates")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+2Bu0_JhMY7BkMmY1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
